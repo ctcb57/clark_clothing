d760e8d4aaea24b74773be8df862885998df3a35
charles clark <charles@pop-os.localdomain>
2020-03-12 23:30:02 -0500

    Created initial homepage setup components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './hompage.component';
+
 function App() {
-  return (
-    <div>Hello World</div>
-  );
+  return <div>
+    <HomePage />
+  </div>;
 }
 
 export default App;
